acd5a3a2a535bc3e07c4fc086ee48a5995d0db82
Victor Alejandro Castro Chavez <a2017113557@estudiantes.upsa.edu.bo>
2021-03-19 21:41:27 -0400

    Se creo la api

---
+++ Resolucion3/Practicando3/Models/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practicando3.Models
+{
+    public class Country
+    {
+        [Key]
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public List<double> Latlng { get; set; }
+        public List<string> Timezones { get; set; }
+        public string Flag { get; set; }
+
+    }
+}
---
+++ Resolucion3/Practicando3/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Practicando3.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Practicando3.Models.Country> Countries { get; set; }
+    }
+}
